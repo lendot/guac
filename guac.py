b5174f4b64518c554b0151925178600523a5a977
Len Lanphar <lanphar@gmail.com>
2017-03-10 10:13:39 -0500

    added support for octave up/down and patch up/down buttons

--- guac.py
+++ guac.py
@@ -7,6 +7,11 @@ import RPi.GPIO as GPIO
 
 import Adafruit_MPR121.MPR121 as MPR121
 
+# octave to start with
+octave=5
+
+# patch to start with
+patch=0
 
 # Create MPR121 instance.
 cap = MPR121.MPR121()
@@ -28,13 +33,21 @@ cap.set_thresholds(16,4)
 pygame.mixer.pre_init(44100, -16, 12, 512)
 pygame.init()
 
-button_pins=[5,6,13,19]
-button_on=[False,False,False,False]
+button_pins={'patch_down' : 19,
+             'patch_up' : 13,
+             'octave_down' : 6,
+             'octave_up' : 5}
+
+#button_pins=[5,6,13,19]
+button_on={'patch_down' : False,
+           'patch_up' : False,
+           'octave_down' : False,
+           'octave_up' : False}
 
 GPIO.setmode(GPIO.BCM)
 
-for pin in button_pins:
-    GPIO.setup(pin,GPIO.IN)
+for pin in button_pins.keys():
+    GPIO.setup(button_pins[pin],GPIO.IN)
 
 
 pygame.midi.init()
@@ -43,14 +56,12 @@ print dev_info
 
 midi=pygame.midi.Output(0)
 
-midi.set_instrument(36)
 
 #NOTE_MAPPING = [60,62,64,65,67,69,71,72,74,76,77,79]
 NOTE_OFFSET = [0,2,4,5,7,9,11,12,14,16,17,19]
 
-# octave to start with
-octave=5
 
+midi.set_instrument(patch)
 
 print('Press Ctrl-C to quit.')
 last_touched = cap.touched()
@@ -70,14 +81,51 @@ while True:
             print('{0} released!'.format(i))
             midi.note_off(octave*12+NOTE_OFFSET[i])
 
-    for i in range(len(button_pins)):
-        if (GPIO.input(button_pins[i]) == True) and not button_on[i]:
-            print("Button {0} pressed!".format(button_pins[i]))
-            button_on[i]=True
-	if (GPIO.input(button_pins[i]) == False) and button_on[i]:
-	    print("Button {0} released!".format(button_pins[i]))
-	    button_on[i]=False 
+
+    # check for patch up and down presses
+    if (GPIO.input(button_pins['patch_up']) == True) and not button_on['patch_up']:
+        print("patch_up pressed")
+        if patch<127:
+            patch += 1
+            print ("New patch: {0}".format(patch))
+            midi.set_instrument(patch)
+        button_on['patch_up']=True
+    if (GPIO.input(button_pins['patch_up']) == False) and button_on['patch_up']:
+        print("patch_up released")
+        button_on['patch_up']=False 
             
+    if (GPIO.input(button_pins['patch_down']) == True) and not button_on['patch_down']:
+        print("patch_down pressed")
+        if patch>0:
+            patch -= 1
+            print ("New patch: {0}".format(patch))
+            midi.set_instrument(patch)
+        button_on['patch_down']=True
+    if (GPIO.input(button_pins['patch_down']) == False) and button_on['patch_down']:
+        print("patch_down released")
+        button_on['patch_down']=False 
+            
+
+    # check for octave up and down presses
+    if (GPIO.input(button_pins['octave_up']) == True) and not button_on['octave_up']:
+        print("octave_up pressed")
+        if octave<8:
+            octave += 1
+            print ("New octave: {0}".format(octave))
+        button_on['octave_up']=True
+    if (GPIO.input(button_pins['octave_up']) == False) and button_on['octave_up']:
+        print("octave_up released")
+        button_on['octave_up']=False 
+            
+    if (GPIO.input(button_pins['octave_down']) == True) and not button_on['octave_down']:
+        print("octave_down pressed")
+        if octave>0:
+            octave -= 1
+            print ("New octave: {0}".format(octave))
+        button_on['octave_down']=True
+    if (GPIO.input(button_pins['octave_down']) == False) and button_on['octave_down']:
+        print("octave_down released")
+        button_on['octave_down']=False 
 
     # Update last state and wait a short period before repeating.
     last_touched = current_touched
